59ada682588d05f34a81425b5902c5abe94b590a
Arghya-n <sakib.hb7@gmail.com>
2024-12-26 10:49:43 +0600

    Backend Initialized

---
+++ BackEnd/EmpTaskAPI/Controllers/ProjectController.cs
@@ -0,0 +1,37 @@
+﻿using EmpTaskAPI.DataAccessLayer;
+using EmpTaskAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmpTaskAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProjectController : ControllerBase
+    {
+        private readonly AppDBContext context;
+
+        public ProjectController(AppDBContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var data = context.Projects.ToList();
+
+            return Ok(data);
+        }
+        [HttpPost]
+
+        public async Task<IActionResult> Create(Project pj)
+        {
+            context.AddAsync(pj);
+            context.SaveChangesAsync();
+            return Ok(pj);
+
+        }
+
+    }
+}
---
+++ BackEnd/EmpTaskAPI/Models/Project.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmpTaskAPI.Models
+{
+    public class Project
+    {
+        [Key]
+        public String ProjectID { get; set; }
+        public String Title { get; set; }
+        public String Description { get; set; }
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+
+
+    }
+}
