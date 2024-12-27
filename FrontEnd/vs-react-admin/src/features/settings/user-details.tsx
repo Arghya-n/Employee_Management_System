3fea96463120e415af991f6cd50b31febd16445a
Alam <u1904102@student.cuet.ac.bd>
2024-12-27 12:09:23 +0600

    Settings form updated

--- FrontEnd/vs-react-admin/src/features/settings/user-details.tsx
+++ FrontEnd/vs-react-admin/src/features/settings/user-details.tsx
@@ -8,19 +8,42 @@ const UserDetails = () => {
     <Card title="Information">
       <Form layout="vertical">
         <Row gutter={24}>
-          <Col span={8}>
+          <Col span={12}>
             <Form.Item label="Name">
               <Input value={user.name} />
             </Form.Item>
           </Col>
-          <Col span={8}>
+          <Col span={12}>
             <Form.Item label="Email">
               <Input value={user.email} />
             </Form.Item>
           </Col>
+          <Col span={12}>
+            <Form.Item label="Password">
+              <Input value={user.password} />
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item label="Phone Number">
+              <Input value={user.phoneNumber} />
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item label="Working Stack">
+              <Input value={user.workingStack} />
+            </Form.Item>
+          </Col>
+          <Col span={12}>
+            <Form.Item label="Role">
+              <Input value={user.role} />
+            </Form.Item>
+          </Col>
+
+
+          
         </Row>
         <Row gutter={24}>
-          <Col span={8}>
+          <Col span={12}>
             <Form.Item label="Status">
               <Tag key='status' color="geekblue">{user.role}</Tag>
             </Form.Item>
