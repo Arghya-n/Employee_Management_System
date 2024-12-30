990d4b62b6d87bc5740b16a9106f2c9e7af11a44
mithi <shamima.mithi.r@gmail.com>
2024-12-30 22:40:51 +0600

    Added Role Selection

--- FrontEnd/vs-react-admin/src/features/auth/login-form.tsx
+++ FrontEnd/vs-react-admin/src/features/auth/login-form.tsx
@@ -1,9 +1,10 @@
 import { useState } from 'react';
-import { Form, Input, Button, Checkbox, Typography } from 'antd';
+import { Form, Input, Button, Checkbox, Typography, Select } from 'antd';
 import { LockOutlined, UnlockOutlined, UserOutlined } from '@ant-design/icons';
 import useAuth from '@hooks/use-auth';
 
 const { Link } = Typography;
+const { Option } = Select;
 
 interface LoginFormProps {
   email: string;
@@ -17,10 +18,10 @@ const LoginForm = () => {
   const onFinish = (values: LoginFormProps) => {
     onLogin({
       email: values.email,
-      password: values.password
+      password: values.password,
     });
   };
-  
+
   return (
     <Form
       layout="vertical"
@@ -33,6 +34,19 @@ const LoginForm = () => {
       onFinish={onFinish}
       style={{ width: '100%' }}
     >
+      {/* Role Selection */}
+      <Form.Item
+        name="role"
+        label="Log in as"
+        rules={[{ required: true, message: 'Selection is required' }]}
+      >
+        <Select placeholder="Select role">
+          <Option value="admin">Admin</Option>
+          <Option value="user">User</Option>
+        </Select>
+      </Form.Item>
+
+      {/* Email Field */}
       <Form.Item
         name="email"
         label="Email"
@@ -40,6 +54,8 @@ const LoginForm = () => {
       >
         <Input prefix={<UserOutlined />} placeholder="Email" />
       </Form.Item>
+
+      {/* Password Field */}
       <Form.Item
         name="password"
         label="Password"
@@ -47,22 +63,26 @@ const LoginForm = () => {
       >
         <Input.Password
           prefix={passwordVisible ? <UnlockOutlined /> : <LockOutlined />}
-          type="password"
           placeholder="Password"
-          visibilityToggle={{ visible: passwordVisible, onVisibleChange: setPasswordVisible }}
+          iconRender={(visible) =>
+            visible ? <UnlockOutlined /> : <LockOutlined />
+          }
+          visibilityToggle
+          onClick={() => setPasswordVisible(!passwordVisible)}
         />
       </Form.Item>
+
+      {/* Remember Me and Forgot Password */}
       <Form.Item>
         <div className="flex items-center justify-between">
           <Form.Item name="remember" valuePropName="checked" noStyle>
             <Checkbox>Remember me</Checkbox>
           </Form.Item>
-          
-          <Link href={'/forget-password'}>
-            Forgot password
-          </Link>
+          <Link href="/forget-password">Forgot password</Link>
         </div>
       </Form.Item>
+
+      {/* Submit Button */}
       <Form.Item>
         <Button loading={isLoading} type="primary" htmlType="submit" block>
           Log in
